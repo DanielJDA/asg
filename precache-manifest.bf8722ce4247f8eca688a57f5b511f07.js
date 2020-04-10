a45bab556fa8c04e9ed7d29a84816149afa13edb
DanielJDA <abrego@DESKTOP-40O0HLM.localdomain>
2020-04-09 22:50:35 -0600

    asg

---
+++ precache-manifest.bf8722ce4247f8eca688a57f5b511f07.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88440e9fe6370d79ba08393216f150b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c366b30861304f3ed80",
+    "url": "./static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "0b38dbb30a08bab8c5b8",
+    "url": "./static/css/main.0060b9d8.chunk.css"
+  },
+  {
+    "revision": "7c366b30861304f3ed80",
+    "url": "./static/js/2.b601fb7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b601fb7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b38dbb30a08bab8c5b8",
+    "url": "./static/js/main.db7054b2.chunk.js"
+  },
+  {
+    "revision": "fdcba46aa15c34e18bca",
+    "url": "./static/js/runtime-main.9ad30e64.js"
+  },
+  {
+    "revision": "cb8cc3e45c0ed2e86d7c547bad7d6de1",
+    "url": "./static/media/1.cb8cc3e4.png"
+  },
+  {
+    "revision": "5a4c207d1fbb01eda68d7be239b941aa",
+    "url": "./static/media/2.5a4c207d.png"
+  },
+  {
+    "revision": "b039bb22682ba47c78a2cf5761f58eb1",
+    "url": "./static/media/Logo.b039bb22.svg"
+  },
+  {
+    "revision": "c67f2c42eb4217f3ee513b32969e9a08",
+    "url": "./static/media/fondo-01.c67f2c42.svg"
+  },
+  {
+    "revision": "ad6c6b962911afba3d2aa69ae16ecabd",
+    "url": "./static/media/mouse.ad6c6b96.svg"
+  }
+]);
